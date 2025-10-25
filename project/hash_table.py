6ec5a09c0f5b862fd58a3c4a9a8f12dcb39e59fd
Kirill Churkin <st129364@student.spbu.ru>
2025-10-25 18:26:04 +0300

    Add a type hints

--- project/hash_table.py
+++ project/hash_table.py
@@ -1,18 +1,21 @@
+from typing import Any, List, Optional, Iterator, Tuple
+
+
 class HashTable:
     """A simple hash table implementation using separate chaining with doubly linked lists"""
 
-    def __init__(self, size=10):
-        self.size = size
-        self.table = [None] * size
-        self._length = 0
+    def __init__(self, size: int = 10) -> None:
+        self.size: int = size
+        self.table: List[Optional["_LinkedList"]] = [None] * size
+        self._length: int = 0
 
-    def _hash(self, key):
+    def _hash(self, key: Any) -> int:
         """Compute the hash value for a key"""
         return hash(key) % self.size
 
-    def __setitem__(self, key, value):
+    def __setitem__(self, key: Any, value: Any) -> None:
         """Set the value for a key using table[key] = value syntax"""
-        index = self._hash(key)
+        index: int = self._hash(key)
 
         if self.table[index] is None:
             self.table[index] = _LinkedList()
@@ -20,22 +23,22 @@ class HashTable:
         if self.table[index].insert(key, value):
             self._length += 1
 
-    def __getitem__(self, key):
+    def __getitem__(self, key: Any) -> Any:
         """Get the value for a key using table[key] syntax"""
-        index = self._hash(key)
+        index: int = self._hash(key)
 
         if self.table[index] is None:
             raise KeyError(f"Key '{key}' not found")
 
-        value = self.table[index].find(key)
+        value: Optional[Any] = self.table[index].find(key)
         if value is None:
             raise KeyError(f"Key '{key}' not found")
 
         return value
 
-    def __delitem__(self, key):
+    def __delitem__(self, key: Any) -> None:
         """Delete a key-value pair using del table[key] syntax"""
-        index = self._hash(key)
+        index: int = self._hash(key)
 
         if self.table[index] is None:
             raise KeyError(f"Key '{key}' not found")
@@ -45,7 +48,7 @@ class HashTable:
         else:
             raise KeyError(f"Key '{key}' not found")
 
-    def __contains__(self, key):
+    def __contains__(self, key: Any) -> bool:
         """Check if a key exists in the hash table using 'key in table' syntax"""
         try:
             _ = self[key]
@@ -53,40 +56,40 @@ class HashTable:
         except KeyError:
             return False
 
-    def __len__(self):
+    def __len__(self) -> int:
         """Return the number of key-value pairs in the hash table"""
         return self._length
 
-    def __iter__(self):
+    def __iter__(self) -> Iterator[Any]:
         """Return a forward iterator for keys"""
         return self._ForwardIterator(self)
 
-    def keys(self):
+    def keys(self) -> List[Any]:
         """Return a list of all keys in the hash table"""
         return [key for key in self]
 
-    def values(self):
+    def values(self) -> List[Any]:
         """Return a list of all values in the hash table"""
         return [self[key] for key in self]
 
-    def items(self):
+    def items(self) -> List[Tuple[Any, Any]]:
         """Return a list of all key-value pairs in the hash table"""
         return [(key, self[key]) for key in self]
 
-    def reverse_iter(self):
+    def reverse_iter(self) -> "_ReverseIterator":
         """Return a reverse iterator for keys"""
         return self._ReverseIterator(self)
 
     class _ForwardIterator:
         """Forward iterator for traversing the hash table"""
 
-        def __init__(self, hash_table):
-            self.hash_table = hash_table
-            self.bucket_index = 0
-            self.current_node = None
+        def __init__(self, hash_table: "HashTable") -> None:
+            self.hash_table: "HashTable" = hash_table
+            self.bucket_index: int = 0
+            self.current_node: Optional["_Node"] = None
             self._find_first_element()
 
-        def _find_first_element(self):
+        def _find_first_element(self) -> None:
             """Find the first non-empty element in the hash table"""
             while self.bucket_index < self.hash_table.size and (
                 self.hash_table.table[self.bucket_index] is None
@@ -97,14 +100,14 @@ class HashTable:
             if self.bucket_index < self.hash_table.size:
                 self.current_node = self.hash_table.table[self.bucket_index].head
 
-        def __iter__(self):
+        def __iter__(self) -> Iterator[Any]:
             return self
 
-        def __next__(self):
+        def __next__(self) -> Any:
             if self.current_node is None:
                 raise StopIteration
 
-            key = self.current_node.key
+            key: Any = self.current_node.key
             self.current_node = self.current_node.next
 
             if self.current_node is None:
@@ -116,13 +119,13 @@ class HashTable:
     class _ReverseIterator:
         """Reverse iterator for traversing the hash table from the end"""
 
-        def __init__(self, hash_table):
-            self.hash_table = hash_table
-            self.bucket_index = hash_table.size - 1
-            self.current_node = None
+        def __init__(self, hash_table: "HashTable") -> None:
+            self.hash_table: "HashTable" = hash_table
+            self.bucket_index: int = hash_table.size - 1
+            self.current_node: Optional["_Node"] = None
             self._find_last_element()
 
-        def _find_last_element(self):
+        def _find_last_element(self) -> None:
             """Find the last non-empty element in the hash table"""
             while self.bucket_index >= 0 and (
                 self.hash_table.table[self.bucket_index] is None
@@ -133,14 +136,14 @@ class HashTable:
             if self.bucket_index >= 0:
                 self.current_node = self.hash_table.table[self.bucket_index].tail
 
-        def __iter__(self):
+        def __iter__(self) -> Iterator[Any]:
             return self
 
-        def __next__(self):
+        def __next__(self) -> Any:
             if self.current_node is None:
                 raise StopIteration
 
-            key = self.current_node.key
+            key: Any = self.current_node.key
             self.current_node = self.current_node.prev
 
             if self.current_node is None:
@@ -153,23 +156,23 @@ class HashTable:
 class _Node:
     """A node in the doubly linked list"""
 
-    def __init__(self, key, value):
-        self.key = key
-        self.value = value
-        self.next = None
-        self.prev = None
+    def __init__(self, key: Any, value: Any) -> None:
+        self.key: Any = key
+        self.value: Any = value
+        self.next: Optional["_Node"] = None
+        self.prev: Optional["_Node"] = None
 
 
 class _LinkedList:
     """Doubly linked list for collision resolution"""
 
-    def __init__(self):
-        self.head = None
-        self.tail = None
+    def __init__(self) -> None:
+        self.head: Optional["_Node"] = None
+        self.tail: Optional["_Node"] = None
 
-    def insert(self, key, value):
+    def insert(self, key: Any, value: Any) -> bool:
         """Insert or update a key-value pair in the list"""
-        current = self.head
+        current: Optional["_Node"] = self.head
 
         while current is not None:
             if current.key == key:
@@ -177,21 +180,22 @@ class _LinkedList:
                 return False
             current = current.next
 
-        new_node = _Node(key, value)
+        new_node: "_Node" = _Node(key, value)
 
         if self.head is None:
             self.head = new_node
             self.tail = new_node
         else:
             new_node.prev = self.tail
-            self.tail.next = new_node
+            if self.tail is not None:
+                self.tail.next = new_node
             self.tail = new_node
 
         return True
 
-    def find(self, key):
+    def find(self, key: Any) -> Optional[Any]:
         """Find a value by key in the list"""
-        current = self.head
+        current: Optional["_Node"] = self.head
 
         while current is not None:
             if current.key == key:
@@ -200,9 +204,9 @@ class _LinkedList:
 
         return None
 
-    def remove(self, key):
+    def remove(self, key: Any) -> bool:
         """Remove a key-value pair from the list"""
-        current = self.head
+        current: Optional["_Node"] = self.head
 
         while current is not None:
             if current.key == key:
