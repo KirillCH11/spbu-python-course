64362f1bb812da3f83772dcd18147cbf2179eb65
Kirill Churkin <st129364@student.spbu.ru>
2025-10-31 19:53:18 +0300

    Create a correct structure - BucketList

--- project/hash_table.py
+++ project/hash_table.py
@@ -1,58 +1,100 @@
-from typing import Any, List, Optional, Iterator, Tuple
+from typing import Any, List, Tuple, Iterator, Optional
 
 
 class HashTable:
-    """A simple hash table implementation using separate chaining with doubly linked lists"""
+    """
+    A hash table implemented as a doubly linked list of buckets.
+    Each bucket contains a doubly linked list for collision resolution.
+
+    Supports dictionary-like interface with [] syntax.
+    """
 
     def __init__(self, size: int = 10) -> None:
+        """
+        Initialize hash table with specified size.
+
+        Args:
+            size: Number of buckets in the hash table
+        """
         self.size: int = size
-        self.table: List[Optional["_LinkedList"]] = [None] * size
+        self.buckets: "BucketList" = BucketList(size)
         self._length: int = 0
 
     def _hash(self, key: Any) -> int:
-        """Compute the hash value for a key"""
+        """
+        Compute bucket index for a key.
+
+        Args:
+            key: Key to hash
+
+        Returns:
+            Bucket index between 0 and size-1
+        """
         return hash(key) % self.size
 
     def __setitem__(self, key: Any, value: Any) -> None:
-        """Set the value for a key using table[key] = value syntax"""
+        """
+        Set key-value pair. Supports table[key] = value syntax.
+
+        Args:
+            key: Key to set
+            value: Value to associate with key
+        """
         index: int = self._hash(key)
+        bucket: "Bucket" = self.buckets.get_bucket(index)
 
-        if self.table[index] is None:
-            self.table[index] = _LinkedList()
-
-        linked_list = self.table[index]
-        if linked_list is not None and linked_list.insert(key, value):
+        if bucket.collision_list.insert(key, value):
             self._length += 1
 
     def __getitem__(self, key: Any) -> Any:
-        """Get the value for a key using table[key] syntax"""
+        """
+        Get value for key. Supports value = table[key] syntax.
+
+        Args:
+            key: Key to look up
+
+        Returns:
+            Value associated with key
+
+        Raises:
+            KeyError: If key is not found
+        """
         index: int = self._hash(key)
+        bucket: "Bucket" = self.buckets.get_bucket(index)
 
-        linked_list = self.table[index]
-        if linked_list is None:
-            raise KeyError(f"Key '{key}' not found")
-
-        value: Optional[Any] = linked_list.find(key)
+        value: Optional[Any] = bucket.collision_list.find(key)
         if value is None:
             raise KeyError(f"Key '{key}' not found")
-
         return value
 
     def __delitem__(self, key: Any) -> None:
-        """Delete a key-value pair using del table[key] syntax"""
+        """
+        Delete key-value pair. Supports del table[key] syntax.
+
+        Args:
+            key: Key to delete
+
+        Raises:
+            KeyError: If key is not found
+        """
         index: int = self._hash(key)
+        bucket: "Bucket" = self.buckets.get_bucket(index)
 
-        linked_list = self.table[index]
-        if linked_list is None:
-            raise KeyError(f"Key '{key}' not found")
-
-        if linked_list.remove(key):
+        if bucket.collision_list.remove(key):
             self._length -= 1
         else:
             raise KeyError(f"Key '{key}' not found")
 
     def __contains__(self, key: Any) -> bool:
-        """Check if a key exists in the hash table using 'key in table' syntax"""
+        """
+        Check if key exists. Supports key in table syntax.
+
+        Args:
+            key: Key to check
+
+        Returns:
+            True if key exists, False otherwise
+        """
         try:
             _ = self[key]
             return True
@@ -60,135 +102,203 @@ class HashTable:
             return False
 
     def __len__(self) -> int:
-        """Return the number of key-value pairs in the hash table"""
+        """
+        Get number of key-value pairs.
+
+        Returns:
+            Number of elements in hash table
+        """
         return self._length
 
     def __iter__(self) -> Iterator[Any]:
-        """Return a forward iterator for keys"""
-        return self._ForwardIterator(self)
+        """
+        Iterate over keys in hash table (forward direction).
+
+        Returns:
+            Iterator over all keys
+        """
+        # Traversal of an external doubly linked list (buckets)
+        current_bucket: Optional[Bucket] = self.buckets.head
+        while current_bucket is not None:
+            # Traversal of an internal doubly linked list (collisions)
+            current_node: Optional[Node] = current_bucket.collision_list.head
+            while current_node is not None:
+                yield current_node.key
+                current_node = current_node.next
+            current_bucket = current_bucket.next
+
+    def reverse_iter(self) -> Iterator[Any]:
+        """
+        Iterate over keys in hash table (reverse direction).
+
+        Returns:
+            Iterator over all keys from end to start
+        """
+        # Backtracking of an external doubly linked list (buckets)
+        current_bucket: Optional[Bucket] = self.buckets.tail
+        while current_bucket is not None:
+            # Backtracking of an internal doubly linked list (collisions)
+            current_node: Optional[Node] = current_bucket.collision_list.tail
+            while current_node is not None:
+                yield current_node.key
+                current_node = current_node.prev
+            current_bucket = current_bucket.prev
 
     def keys(self) -> List[Any]:
-        """Return a list of all keys in the hash table"""
-        return [key for key in self]
+        """
+        Get all keys in hash table.
+
+        Returns:
+            List of all keys
+        """
+        return list(self)
 
     def values(self) -> List[Any]:
-        """Return a list of all values in the hash table"""
+        """
+        Get all values in hash table.
+
+        Returns:
+            List of all values
+        """
         return [self[key] for key in self]
 
     def items(self) -> List[Tuple[Any, Any]]:
-        """Return a list of all key-value pairs in the hash table"""
+        """
+        Get all key-value pairs in hash table.
+
+        Returns:
+            List of (key, value) tuples
+        """
         return [(key, self[key]) for key in self]
 
-    def reverse_iter(self) -> "_ReverseIterator":
-        """Return a reverse iterator for keys"""
-        return self._ReverseIterator(self)
 
-    class _ForwardIterator:
-        """Forward iterator for traversing the hash table"""
+class Bucket:
+    """
+    A bucket in the hash table, representing one cell.
+    Contains a doubly linked list for collision resolution.
+    """
 
-        def __init__(self, hash_table: "HashTable") -> None:
-            self.hash_table: "HashTable" = hash_table
-            self.bucket_index: int = 0
-            self.current_node: Optional["_Node"] = None
-            self._find_first_element()
+    def __init__(self, index: int) -> None:
+        """
+        Initialize bucket with index and empty collision list.
 
-        def _find_first_element(self) -> None:
-            """Find the first non-empty element in the hash table"""
-            while self.bucket_index < self.hash_table.size:
-                linked_list = self.hash_table.table[self.bucket_index]
-                if linked_list is not None and linked_list.head is not None:
-                    self.current_node = linked_list.head
-                    return
-                self.bucket_index += 1
-
-            self.current_node = None
-
-        def __iter__(self) -> Iterator[Any]:
-            return self
-
-        def __next__(self) -> Any:
-            if self.current_node is None:
-                raise StopIteration
-
-            key: Any = self.current_node.key
-            self.current_node = self.current_node.next
-
-            if self.current_node is None:
-                self.bucket_index += 1
-                self._find_first_element()
-
-            return key
-
-    class _ReverseIterator:
-        """Reverse iterator for traversing the hash table from the end"""
-
-        def __init__(self, hash_table: "HashTable") -> None:
-            self.hash_table: "HashTable" = hash_table
-            self.bucket_index: int = hash_table.size - 1
-            self.current_node: Optional["_Node"] = None
-            self._find_last_element()
-
-        def _find_last_element(self) -> None:
-            """Find the last non-empty element in the hash table"""
-            while self.bucket_index >= 0:
-                linked_list = self.hash_table.table[self.bucket_index]
-                if linked_list is not None and linked_list.tail is not None:
-                    self.current_node = linked_list.tail
-                    return
-                self.bucket_index -= 1
-
-            self.current_node = None
-
-        def __iter__(self) -> Iterator[Any]:
-            return self
-
-        def __next__(self) -> Any:
-            if self.current_node is None:
-                raise StopIteration
-
-            key: Any = self.current_node.key
-            self.current_node = self.current_node.prev
-
-            if self.current_node is None:
-                self.bucket_index -= 1
-                self._find_last_element()
-
-            return key
+        Args:
+            index: Index of this bucket in the table
+        """
+        self.index: int = index
+        self.collision_list: "LinkedList" = (
+            LinkedList()
+        )  # An internal doubly linked list
+        self.next: Optional["Bucket"] = None  # Link to the next bucket
+        self.prev: Optional["Bucket"] = None  # Link to the previous bucket
 
 
-class _Node:
-    """A node in the doubly linked list"""
+class BucketList:
+    """
+    Outer doubly linked list representing all buckets in the hash table.
+    This makes the table itself a doubly linked list.
+    """
+
+    def __init__(self, size: int) -> None:
+        """
+        Initialize the bucket list with specified number of buckets.
+
+        Args:
+            size: Number of buckets to create
+        """
+        self.head: Optional[Bucket] = None
+        self.tail: Optional[Bucket] = None
+        self._buckets: List[Bucket] = []  # For quick index access
+
+        # Creating a doubly linked list of batches
+        for i in range(size):
+            new_bucket = Bucket(i)
+            self._buckets.append(new_bucket)
+
+            if self.head is None:
+                self.head = new_bucket
+                self.tail = new_bucket
+            else:
+                new_bucket.prev = self.tail
+                if self.tail is not None:
+                    self.tail.next = new_bucket
+                self.tail = new_bucket
+
+    def get_bucket(self, index: int) -> "Bucket":
+        """
+        Get bucket by index.
+
+        Args:
+            index: Bucket index (0 to size-1)
+
+        Returns:
+            Bucket at specified index
+        """
+        return self._buckets[index]
+
+
+class Node:
+    """
+    A node in the doubly linked list for collision resolution.
+
+    Each node stores a key-value pair and references to next and previous nodes.
+    """
 
     def __init__(self, key: Any, value: Any) -> None:
+        """
+        Initialize node with key and value.
+
+        Args:
+            key: Key for the node
+            value: Value for the node
+        """
         self.key: Any = key
         self.value: Any = value
-        self.next: Optional["_Node"] = None
-        self.prev: Optional["_Node"] = None
+        self.next: Optional["Node"] = None
+        self.prev: Optional["Node"] = None
 
 
-class _LinkedList:
-    """Doubly linked list for collision resolution"""
+class LinkedList:
+    """
+    Doubly linked list for collision resolution in a bucket.
+
+    Supports forward and reverse iteration.
+    """
 
     def __init__(self) -> None:
-        self.head: Optional["_Node"] = None
-        self.tail: Optional["_Node"] = None
+        """Initialize empty linked list."""
+        self.head: Optional[Node] = None
+        self.tail: Optional[Node] = None
 
     def insert(self, key: Any, value: Any) -> bool:
-        """Insert or update a key-value pair in the list"""
-        current: Optional["_Node"] = self.head
+        """
+        Insert or update key-value pair in list.
 
+        Args:
+            key: Key to insert or update
+            value: Value to associate with key
+
+        Returns:
+            True if new key was inserted, False if existing key was updated
+        """
+        current: Optional[Node] = self.head
+
+        # Search for existing key
         while current is not None:
             if current.key == key:
                 current.value = value
                 return False
             current = current.next
 
-        new_node: "_Node" = _Node(key, value)
+        # Key not found - insert new node
+        new_node = Node(key, value)
 
         if self.head is None:
+            # First node in list
             self.head = new_node
             self.tail = new_node
         else:
+            # Append to end of list
             new_node.prev = self.tail
             if self.tail is not None:
                 self.tail.next = new_node
@@ -197,8 +307,16 @@ class _LinkedList:
         return True
 
     def find(self, key: Any) -> Optional[Any]:
-        """Find a value by key in the list"""
-        current: Optional["_Node"] = self.head
+        """
+        Find value for key in list.
+
+        Args:
+            key: Key to find
+
+        Returns:
+            Value if key found, None otherwise
+        """
+        current: Optional[Node] = self.head
 
         while current is not None:
             if current.key == key:
@@ -208,16 +326,26 @@ class _LinkedList:
         return None
 
     def remove(self, key: Any) -> bool:
-        """Remove a key-value pair from the list"""
-        current: Optional["_Node"] = self.head
+        """
+        Remove key-value pair from list.
+
+        Args:
+            key: Key to remove
+
+        Returns:
+            True if key was found and removed, False otherwise
+        """
+        current: Optional[Node] = self.head
 
         while current is not None:
             if current.key == key:
+                # Update previous node's next pointer
                 if current.prev is not None:
                     current.prev.next = current.next
                 else:
                     self.head = current.next
 
+                # Update next node's previous pointer
                 if current.next is not None:
                     current.next.prev = current.prev
                 else:
@@ -228,3 +356,27 @@ class _LinkedList:
             current = current.next
 
         return False
+
+    def __iter__(self) -> Iterator[Any]:
+        """
+        Iterate over keys in forward direction.
+
+        Returns:
+            Iterator over keys from head to tail
+        """
+        current: Optional[Node] = self.head
+        while current is not None:
+            yield current.key
+            current = current.next
+
+    def reverse_iter(self) -> Iterator[Any]:
+        """
+        Iterate over keys in reverse direction.
+
+        Returns:
+            Iterator over keys from tail to head
+        """
+        current: Optional[Node] = self.tail
+        while current is not None:
+            yield current.key
+            current = current.prev
--- tests/test_hash_table.py
+++ tests/test_hash_table.py
@@ -1,12 +1,15 @@
 import pytest
-from project.hash_table import HashTable
+from project.hash_table import HashTable, LinkedList
 
 
 class TestHashTable:
-    def test_basic_operations(self):
-        """Test basic hash table operations"""
+    """Test cases for HashTable class."""
+
+    def test_basic_operations(self) -> None:
+        """Test basic hash table operations."""
         table = HashTable()
 
+        # Test insertion and retrieval
         table["name"] = "Kirill"
         table["age"] = 19
         table["city"] = "St. Petersburg"
@@ -15,22 +18,24 @@ class TestHashTable:
         assert table["age"] == 19
         assert table["city"] == "St. Petersburg"
 
+        # Test length
         assert len(table) == 3
 
+        # Test contains
         assert "name" in table
         assert "country" not in table
 
-    def test_update_value(self):
-        """Test value update functionality"""
+    def test_update_value(self) -> None:
+        """Test updating existing key."""
         table = HashTable()
         table["key"] = "old_value"
-        table["key"] = "new_value"
+        table["key"] = "new_value"  # Update
 
         assert table["key"] == "new_value"
-        assert len(table) == 1
+        assert len(table) == 1  # Length should not change
 
-    def test_key_error(self):
-        """Test KeyError for non-existent keys"""
+    def test_key_error(self) -> None:
+        """Test KeyError for non-existent keys."""
         table = HashTable()
 
         with pytest.raises(KeyError):
@@ -39,8 +44,8 @@ class TestHashTable:
         with pytest.raises(KeyError):
             del table["nonexistent"]
 
-    def test_deletion(self):
-        """Test element deletion"""
+    def test_deletion(self) -> None:
+        """Test element deletion."""
         table = HashTable()
         table["a"] = 1
         table["b"] = 2
@@ -51,21 +56,46 @@ class TestHashTable:
         assert "b" in table
         assert len(table) == 1
 
-    def test_collision_resolution(self):
-        """Test collision resolution mechanism"""
-        table = HashTable(size=2)
+    def test_collision_resolution(self) -> None:
+        """
+        Test collision resolution with small table size.
 
+        This test forces collisions by using a very small table size (2).
+        Multiple keys will hash to the same bucket, testing the linked list.
+        """
+        table = HashTable(size=2)  # Small size to guarantee collisions
+
+        # These keys will likely collide in 2 buckets
         table["x"] = 1
         table["y"] = 2
         table["z"] = 3
+        table["w"] = 4
 
+        # Verify all values can be retrieved correctly
         assert table["x"] == 1
         assert table["y"] == 2
         assert table["z"] == 3
-        assert len(table) == 3
+        assert table["w"] == 4
 
-    def test_forward_iterator(self):
-        """Test forward iterator functionality"""
+        # Verify length is correct
+        assert len(table) == 4
+
+        # Verify all keys are accessible
+        assert "x" in table
+        assert "y" in table
+        assert "z" in table
+        assert "w" in table
+
+        # Test deletion with collisions
+        del table["y"]
+        assert "y" not in table
+        assert len(table) == 3
+        assert table["x"] == 1  # Other keys should still work
+        assert table["z"] == 3
+        assert table["w"] == 4
+
+    def test_iteration(self) -> None:
+        """Test forward iteration."""
         table = HashTable()
         table["a"] = 1
         table["b"] = 2
@@ -77,21 +107,8 @@ class TestHashTable:
 
         assert set(keys) == {"a", "b", "c"}
 
-    def test_reverse_iterator(self):
-        """Test reverse iterator functionality"""
-        table = HashTable()
-        table["a"] = 1
-        table["b"] = 2
-        table["c"] = 3
-
-        keys = []
-        for key in table.reverse_iter():
-            keys.append(key)
-
-        assert set(keys) == {"a", "b", "c"}
-
-    def test_keys_values_items(self):
-        """Test keys(), values(), and items() methods"""
+    def test_keys_values_items(self) -> None:
+        """Test keys(), values(), and items() methods."""
         table = HashTable()
         table["a"] = 1
         table["b"] = 2
@@ -100,13 +117,108 @@ class TestHashTable:
         assert set(table.values()) == {1, 2}
         assert set(table.items()) == {("a", 1), ("b", 2)}
 
-    def test_empty_table(self):
-        """Test behavior of empty hash table"""
+    def test_empty_table(self) -> None:
+        """Test behavior of empty hash table."""
         table = HashTable()
 
         assert len(table) == 0
         assert list(table) == []
-        assert list(table.reverse_iter()) == []
         assert table.keys() == []
         assert table.values() == []
         assert table.items() == []
+
+
+class TestLinkedList:
+    """Test cases for LinkedList class."""
+
+    def test_linked_list_operations(self) -> None:
+        """Test basic linked list operations."""
+        lst = LinkedList()
+
+        # Test insertion
+        assert lst.insert("a", 1) == True
+        assert lst.insert("b", 2) == True
+        assert lst.insert("a", 3) == False  # Update existing
+
+        # Test find
+        assert lst.find("a") == 3
+        assert lst.find("b") == 2
+        assert lst.find("c") is None
+
+        # Test remove
+        assert lst.remove("a") == True
+        assert lst.remove("c") == False
+        assert lst.find("a") is None
+
+    def test_forward_iteration(self) -> None:
+        """Test forward iteration in linked list."""
+        lst = LinkedList()
+        lst.insert("a", 1)
+        lst.insert("b", 2)
+        lst.insert("c", 3)
+
+        keys = list(lst)
+        assert keys == ["a", "b", "c"]
+
+    def test_reverse_iteration(self) -> None:
+        """Test reverse iteration in linked list."""
+        lst = LinkedList()
+        lst.insert("a", 1)
+        lst.insert("b", 2)
+        lst.insert("c", 3)
+
+        keys = list(lst.reverse_iter())
+        assert keys == ["c", "b", "a"]
+
+    def test_empty_list(self) -> None:
+        """Test behavior of empty linked list."""
+        lst = LinkedList()
+
+        assert list(lst) == []
+        assert list(lst.reverse_iter()) == []
+        assert lst.find("any") is None
+        assert lst.remove("any") == False
+
+
+def test_doubly_linked_table_structure():
+    """Test that hash table uses doubly linked lists for buckets."""
+    table = HashTable(size=3)
+
+    # Check that buckets are connected in a doubly linked list
+    assert table.buckets.head is not None
+    assert table.buckets.tail is not None
+
+    # Check connections between buckets
+    bucket1 = table.buckets.head
+    bucket2 = bucket1.next
+    bucket3 = bucket2.next
+
+    assert bucket1.index == 0
+    assert bucket2.index == 1
+    assert bucket3.index == 2
+
+    # Check reverse connections
+    assert bucket3.prev == bucket2
+    assert bucket2.prev == bucket1
+    assert bucket1.prev is None
+
+    # Check data operations work correctly
+    table["test"] = "value"
+    assert "test" in table
+    assert table["test"] == "value"
+
+
+def test_table_iteration_through_linked_buckets():
+    """Test iteration goes through linked buckets properly."""
+    table = HashTable(size=2)
+
+    table["a"] = 1
+    table["b"] = 2
+    table["c"] = 3
+
+    # Forward iteration through doubly linked lists
+    keys_forward = list(table)
+    keys_reverse = list(table.reverse_iter())
+
+    assert set(keys_forward) == {"a", "b", "c"}
+    assert set(keys_reverse) == {"a", "b", "c"}
